eaffd09763ddea3bee9a353f04d42cf3ce9de6ec
drdlee <danielrudi.lee@gmail.com>
2018-02-06 11:02:33 +0800

    unit test on reply, reply replationship: - test user can read replies from single thread - Thread has many replies (Thread.php) -  test reply is instance of user   .php artisan make:test ReplyTest --unit   .Reply.php, make replationship

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -8,25 +8,44 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class ExampleTest extends TestCase
 {
     use RefreshDatabase;
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->thread = factory('App\Thread')->create();
+    }
+    
+    /** @test */
     public function test_a_user_can_browse_threads()
     {
-        $thread = factory('App\Thread')->create();
-
-        $response = $this->get('/threads');
-        $response->assertSee($thread->title);
-
+        $this->get('/threads')
+            ->assertSee($this->thread->title)
+        ;
+        
     }
 
+    /** @test */
     public function test_a_user_can_read_a_single_thread()
     {
-        $thread = factory('App\Thread')->create();
+        $this->get('/threads/'.$this->thread->id)
+            ->assertSee($this->thread->title)
+        ;
+    }
+
+    /** @test */
+    function test_a_user_can_read_replies_from_a_single_thread()
+    {
+        // kita punya thread
+        // $this->thread = factory('App\Thread')->create();
+
+        // threadnya ada replies
+        $reply = factory('App\Reply')->create(['thread_id' => $this->thread->id]);
         
-        $response = $this->get('/threads/'.$thread->id);
-        $response->assertSee($thread->title);
+        // saat kita buka thread tsb
+        $response = $this->get('/threads/'.$this->thread->id);
+
+        // kita bisa lihat replies-nya
+        $response->assertSee($reply->body);
     }
 }
