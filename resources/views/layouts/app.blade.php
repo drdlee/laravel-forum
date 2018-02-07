09b372e4688b2dd886255f7ffb5ecdc6c752defc
drdlee <danielrudi.lee@gmail.com>
2018-02-07 18:15:51 +0800

    Filter by Popular Thread

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,10 +54,12 @@
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Browse <span class="caret"></span></a>
                             <ul class="dropdown-menu">
                                 <li><a href="/threads/">All Threads</a></li>
-                                
+
                                 @if(auth()->check())
                                 <li><a href="/threads/?by={{auth()->user()->name}}">My Threads</a></li>
                                 @endif
+
+                                <li><a href="/threads?popular=1">Popular All Time</a></li>
                             </ul>
                         </li>
                         <li class="dropdown">
